8abd3771738c940fd2829e73d6c03b32f75b9f90
Soham Nakhare <soham.nakhare@shippit.com>
2023-04-01 12:36:50 +0530

    remove pointer events from follow cursor

--- ui/components/FollowCursor/FollowCursor.tsx
+++ ui/components/FollowCursor/FollowCursor.tsx
@@ -1,43 +1,27 @@
 import React, { useLayoutEffect, useState } from 'react';
 import styles from './FollowCursor.module.css';
 
-const InfoContainer = ({
-  mouseX,
-  mouseY,
-}: {
-  mouseX: number;
-  mouseY: number;
-}) => (
-  <div
-    style={{
-      position: 'absolute',
-      top: mouseY,
-      left: mouseX,
-      width: '50vw',
-      height: '50vh',
-      border: '1px solid white',
-    }}
-  >
-    <h1>Information</h1>
-  </div>
-);
-
 export const FollowCursor = () => {
   const [mousePosition, setMousePosition] = useState({ mouseX: 0, mouseY: 0 });
   const { mouseX, mouseY } = mousePosition;
 
+  const handleMouseMove = (e: MouseEvent) => {
+    setMousePosition({ mouseX: e.clientX, mouseY: e.clientY });
+  };
+
   useLayoutEffect(() => {
-    window.addEventListener('mousemove', (e) => {
-      setMousePosition({ mouseX: e.clientX, mouseY: e.clientY });
-    });
-  });
+    window.addEventListener('mousemove', handleMouseMove);
+    return () => window.removeEventListener('mousemove', handleMouseMove);
+  }, []);
 
   const dotPosition = () => ({ left: mouseX - 4, top: mouseY - 4 });
 
   return (
     <>
-      <InfoContainer mouseX={mouseX} mouseY={mouseY} />
-      <span style={dotPosition()} className={styles.dot}></span>
+      <span
+        style={{ ...dotPosition(), pointerEvents: 'none' }}
+        className={styles.dot}
+      ></span>
     </>
   );
 };
